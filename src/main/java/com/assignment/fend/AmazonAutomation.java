358cd82f72e9d94fcb0b2f303c35a4480a99c9bc
Sunansh <65416462+Sunansh@users.noreply.github.com>
2024-07-13 16:02:38 +0530

    Update AmazonAutomation.java

--- src/main/java/com/assignment/fend/AmazonAutomation.java
+++ src/main/java/com/assignment/fend/AmazonAutomation.java
@@ -1,4 +1,14 @@
 package com.assignment.fend;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class AmazonAutomation
   {
     public static void main(String[] args)
