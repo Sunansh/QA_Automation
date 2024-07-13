1dcd4e3293c7f8c65c71945ed1801b07b6f0ee65
Sunansh <65416462+Sunansh@users.noreply.github.com>
2024-07-13 23:22:45 +0530

    Update AmazonAutomation.java

--- src/main/java/com/assignment/fend/AmazonAutomation.java
+++ src/main/java/com/assignment/fend/AmazonAutomation.java
@@ -13,7 +13,7 @@ public class AmazonAutomation
   {
     public static void main(String[] args)
     {
-       System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+       System.setProperty("webdriver.chrome.driver", "C:\\Users\\sunan\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 
         // Create a new instance of the Chrome driver
         WebDriver driver = new ChromeDriver();
@@ -26,37 +26,33 @@ public class AmazonAutomation
             search.submit();
 
             // Wait for results to load
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/span/div/div/div[2]/div/span/a/div/img")));
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/span/div/div/div[1]/div/span/a/div/img")));
 
             // Capture product details
-            WebElement firstProduct = driver.findElement(By.xpath("//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/span/div/div/div[3]/div[2]/div/h2/span"));
-            String productName = firstProduct.findElement(By.xpath("//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/span/div/div/div[3]/div[2]/h2/a/span")).getText();
-            String productPrice = firstProduct.findElement(By.xpath("//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/span/div/div/div[3]/div[4]/div[2]/div[1]/a/span/span[2]/span[2]")).getText();
-            String productLink = firstProduct.findElement(By.xpath("//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/span/div/div/div[3]/div[2]/h2/a/span")).getAttribute("href");
+            WebElement firstProduct = driver.findElement(By.xpath("//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/span/div/div/div[2]/div[2]/div/h2/span"));
+            String productName = firstProduct.findElement(By.xpath("//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/span/div/div/div[2]/div[2]/h2/a/span")).getText();
+            String productPrice = firstProduct.findElement(By.xpath("//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/span/div/div/div[2]/div[3]/div/div[1]/a/span/span[2]/span[2]")).getText();
+            String productLink = firstProduct.findElement(By.xpath("//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/span/div/div/div[2]/div[2]/h2/a")).getAttribute("href");
 
             System.out.println("Product Name: " + productName);
             System.out.println("Product Price: " + productPrice);
             System.out.println("Product Link: " + productLink);
 
             // Click on the first product
-            firstProduct.findElement(By.cssSelector("h2 a")).click();
+            firstProduct.findElement(By.xpath("//*[@id='search']/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/span/div/div/div[2]/div[2]/h2/a/span")).click();
 
             // Navigate to Add to Cart
             wait.until(ExpectedConditions.elementToBeClickable(By.id("add-to-cart-button"))).click();
 
             // Navigate to the Buy Now Screen
-            wait.until(ExpectedConditions.elementToBeClickable(By.id("hlb-view-cart-announce"))).click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.id("proceedToRetailCheckout"))).click();
 
             // Optionally proceed to payment (not performing the actual purchase)
             // The following line would typically take you to the payment gateway if the cart is open
             // driver.findElement(By.id("checkout-button")).click(); // Uncomment if needed
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // Close the browser
+                    // Close the browser
             driver.quit();
         }
     }
-}
